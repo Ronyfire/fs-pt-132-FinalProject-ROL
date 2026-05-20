930c5ea7581d521b6b627d74149ee27b277606f0
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-05-20 15:58:01 +0000

    feat: created Game.jsx

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
