3f18c08f17bb635c4a907b4d12f93152abf30ab3
Oscar Becerra <46764350+oskar089@users.noreply.github.com>
2026-05-22 19:25:51 +0000

    Profile terminado

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 from extensions import bcrypt
+from flask_cors import CORS
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
@@ -19,6 +20,7 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+CORS(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
