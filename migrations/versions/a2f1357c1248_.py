5a8c0cf691af212d3f4eb0408de5dc592fd9515d
Robert Daniel Motisan <robertmotisan@gmail.com>
2026-06-01 16:45:08 +0000

    trabajo finalizado

--- migrations/versions/a2f1357c1248_.py
+++ migrations/versions/a2f1357c1248_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3db35a5b9cb6
+Revision ID: a2f1357c1248
 Revises: 
-Create Date: 2026-05-31 01:38:52.953556
+Create Date: 2026-06-01 16:41:31.539590
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3db35a5b9cb6'
+revision = 'a2f1357c1248'
 down_revision = None
 branch_labels = None
 depends_on = None
